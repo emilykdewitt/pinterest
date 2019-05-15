8d2e81ea37ec0a66ab89404fef18808ac7bc95d3
Emily DeWitt <emilykdewitt@gmail.com>
2019-05-15 16:08:59 -0500

    filter using buttons and display num of pins on button

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -1,10 +1,11 @@
-import boardsData from '../../helpers/data/boardsData';
 import util from '../../helpers/util';
+import boardData from '../../helpers/data/boardsData';
 import pins from '../pins/pins';
+import pinsData from '../../helpers/data/pinsData';
 
 const seePinDiv = (e) => {
   const boardId = e.target.closest('.card').id;
-  console.error('you clicked a button', boardId);
+  console.error('you clicked a button!', boardId);
   document.getElementById('boards-page').classList.add('hide');
   document.getElementById('pins-page').classList.remove('hide');
   pins.initPins(boardId);
@@ -17,13 +18,15 @@ const bindEvents = () => {
   }
 };
 
-const domStringBuilder = (boardCategories) => {
+const writeBoards = (boards) => {
   let domString = '';
-  boardCategories.forEach((board) => {
+  boards.forEach((board) => {
     domString += '<div class="col-3">';
-    domString += `<div id="${board.id}" class="card p-2 mt-2">`;
-    domString += `<h4 class="card-title">${board.name}</h4>`;
-    domString += '<button class="btn btn-primary see-pins">Pins</button>';
+    domString += `<div id='${board.id}' class="card p-2">`;
+    domString += '<div class="card-body">';
+    domString += `<h5 class="card-title">${board.name}</h5>`;
+    domString += `<button class="btn btn-warning see-pins">${board.pins.length} Pins</button>`;
+    domString += '</div>';
     domString += '</div>';
     domString += '</div>';
   });
@@ -32,12 +35,12 @@ const domStringBuilder = (boardCategories) => {
 };
 
 const initBoards = () => {
-  boardsData.loadBoards()
-    .then((response) => {
-      const boardResults = response.data.boards;
-      domStringBuilder(boardResults);
+  boardData.loadBoards()
+    .then(resp => pinsData.getPinsForEachBoard(resp.data.boards))
+    .then((boardsWithPins) => {
+      writeBoards(boardsWithPins);
     })
-    .catch(err => console.error('error from loadBoards', err));
+    .catch(err => console.error('error from initBoards requests', err));
 };
 
 export default { initBoards };
